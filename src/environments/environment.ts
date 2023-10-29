4649cd97784207e4c0fe5e88f2c8124ad6fcb187
San3 <98056078+sandroga@users.noreply.github.com>
2023-10-29 11:58:08 +0100

    environment überprüft

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,10 @@
 export const environment = {
-  production: true,
+  production: false,
   backendUrl: '/api',
   firebaseConfig: {
-    apiKey: 'AIzaSyCHKc-9GFvrbXMKLlgua2yAtgCibPLddh8',
-    authDomain: 'budget-planner-7eddb.firebaseapp.com',
-    projectId: 'budget-planner-7eddb',
-    appId: '1:278151755192:web:227ae9460314c348be4b4d',
-  },
-};
-
-// Konfiguration für den Entwicklungsmodus
-if (!environment.production) {
-  environment.firebaseConfig = {
     apiKey: "AIzaSyCHKc-9GFvrbXMKLlgua2yAtgCibPLddh8",
     authDomain: "budget-planner-7eddb.firebaseapp.com",
     projectId: "budget-planner-7eddb",
     appId: "1:278151755192:web:227ae9460314c348be4b4d",
-  };
-}
-
-console.log("Aktuelle Umgebungskonfiguration: ", environment);
+  }
+};
