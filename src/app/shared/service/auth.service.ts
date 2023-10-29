26090a71147b26ce4828aff3eef4922c3e47c7c5
San3 <98056078+sandroga@users.noreply.github.com>
2023-10-29 11:47:30 +0100

    überprüft aufgrund debugging

--- src/app/shared/service/auth.service.ts
+++ src/app/shared/service/auth.service.ts
@@ -8,44 +8,37 @@ import {loginPath} from '../routes';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(
-    private readonly fireauth: AngularFireAuth,
-    private readonly toast: ToastController,
-    private readonly router: Router,
-  ) {}
-
-  currentUser = (): Observable<firebase.User | null> => this.fireauth.user;
-
-  loginWithGithub = (next?: () => void): void => this.login(new firebase.auth.GithubAuthProvider(), next);
-
-  loginWithGoogle = (next?: () => void): void => this.login(new firebase.auth.GoogleAuthProvider(), next);
-
-  logout = (): void => {
-    this.fireauth.signOut();
-    this.router.navigate([loginPath]);
-  };
-
-  // --------------
-  // Helper methods
-  // --------------
-
-  private displayLoginError = (error: firebase.auth.Error): void => {
-    console.error(error);
-    this.toast
-      .create({
-        message: error.message,
-        duration: 5000,
-        position: 'bottom',
-        color: 'danger',
-        icon: 'alert-circle',
-      })
-      .then((toast) => toast.present());
-  };
-
-  private login = (authProvider: firebase.auth.AuthProvider, next?: () => void): void => {
-    from(this.fireauth.signInWithPopup(authProvider)).subscribe({
-      next: next || (() => {}),
-      error: (error) => this.displayLoginError(error),
-    });
-  };
+    constructor(
+        private readonly fireauth: AngularFireAuth,
+        private readonly toast: ToastController,
+        private readonly router: Router,
+    ) {}
+    currentUser = (): Observable<firebase.User | null> => this.fireauth.user;
+    loginWithGithub = (next?: () => void): void => this.login(new firebase.auth.GithubAuthProvider(), next);
+    loginWithGoogle = (next?: () => void): void => this.login(new firebase.auth.GoogleAuthProvider(), next);
+    logout = (): void => {
+        this.fireauth.signOut();
+        this.router.navigate([loginPath]);
+    };
+    // --------------
+    // Helper methods
+    // --------------
+    private displayLoginError = (error: firebase.auth.Error): void => {
+        console.error(error);
+        this.toast
+            .create({
+                message: error.message,
+                duration: 5000,
+                position: 'bottom',
+                color: 'danger',
+                icon: 'alert-circle',
+            })
+            .then((toast) => toast.present());
+    };
+    private login = (authProvider: firebase.auth.AuthProvider, next?: () => void): void => {
+        from(this.fireauth.signInWithPopup(authProvider)).subscribe({
+            next: next || (() => {}),
+            error: (error) => this.displayLoginError(error),
+        });
+    };
 }
