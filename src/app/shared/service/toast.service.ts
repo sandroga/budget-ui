b096843b6c18f1093aca0a45bc54712d193acd1d
San3 <98056078+sandroga@users.noreply.github.com>
2023-10-29 11:48:48 +0100

    überprüft aufgrund debugging

--- src/app/shared/service/toast.service.ts
+++ src/app/shared/service/toast.service.ts
@@ -2,45 +2,43 @@ import {Injectable} from '@angular/core';
 import {ToastController} from '@ionic/angular';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class ToastService {
-  constructor(private readonly toastController: ToastController) {}
-
-  displayErrorToast(message: string, error: any): void {
-    console.error(message, error);
-    this.toastController
-      .create({
-        message: `${message}. ${error.error?.message || ''}`,
-        duration: 2000,
-        position: 'bottom',
-        icon: 'warning',
-        buttons: [
-          {
-            text: 'Dismiss',
-            role: 'cancel',
-            handler: () => {},
-          },
-        ],
-      })
-      .then((toast) => toast.present());
-  }
-
-  displaySuccessToast(message: string): void {
-    this.toastController
-      .create({
-        message: message,
-        duration: 2000,
-        position: 'bottom',
-        icon: 'checkmark',
-        buttons: [
-          {
-            text: 'Dismiss',
-            role: 'cancel',
-            handler: () => {},
-          },
-        ],
-      })
-      .then((toast) => toast.present());
-  }
+    constructor(private readonly toastController: ToastController) {}
+    displayErrorToast(message: string, error: any): void {
+        console.error(message, error);
+        this.toastController
+            .create({
+                message: `${message}. ${error.error?.message || ''}`,
+                duration: 2000,
+                position: 'bottom',
+                icon: 'warning',
+                buttons: [
+                    {
+                        text: 'Dismiss',
+                        role: 'cancel',
+                        handler: () => {},
+                    },
+                ],
+            })
+            .then((toast) => toast.present());
+    }
+    displaySuccessToast(message: string): void {
+        this.toastController
+            .create({
+                message: message,
+                duration: 2000,
+                position: 'bottom',
+                icon: 'checkmark',
+                buttons: [
+                    {
+                        text: 'Dismiss',
+                        role: 'cancel',
+                        handler: () => {},
+                    },
+                ],
+            })
+            .then((toast) => toast.present());
+    }
 }
