4a700c9e3814d1c06749a20520045fdb30ad191e
San3 <98056078+sandroga@users.noreply.github.com>
2023-10-29 11:24:46 +0100

    category-modal ts überprüft

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -3,62 +3,52 @@ import {ModalController} from '@ionic/angular';
 import {ActionSheetService} from '../../shared/service/action-sheet.service';
 import {filter, finalize, from} from 'rxjs';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {CategoryService} from "../category.service";
 import {ToastService} from "../../shared/service/toast.service";
+import {CategoryService} from "../category.service";
+
+[CategoryService]
 
 @Component({
   selector: 'app-category-modal',
   templateUrl: './category-modal.component.html',
 })
 export class CategoryModalComponent {
-  readonly categoryForm: FormGroup; // Declare the FormGroup variable
-  submitting = false; // Variable to track the submitting state
-
+  readonly categoryForm: FormGroup;
+  submitting = false;
   constructor(
-    private readonly actionSheetService: ActionSheetService,
-    private readonly categoryService: CategoryService,
-    private readonly formBuilder: FormBuilder,
-    private readonly modalCtrl: ModalController,
-    private readonly toastService: ToastService
+      private readonly actionSheetService: ActionSheetService,
+      private readonly categoryService: CategoryService,
+      private readonly formBuilder: FormBuilder,
+      private readonly modalCtrl: ModalController,
+      private readonly toastService: ToastService,
   ) {
-    // Initialize the FormGroup in the constructor
     this.categoryForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.maxLength(40)]],
     });
   }
-
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
   }
-
   save(): void {
     this.submitting = true;
-
     this.categoryService
-      .upsertCategory(this.categoryForm.value)
-      .pipe(
-        finalize(() => (this.submitting = false))
-      )
-      .subscribe({
-        next: () => {
-          this.toastService.displaySuccessToast('Category saved');
-          this.modalCtrl.dismiss(null, 'refresh');
-        },
-        error: (error) => {
-          this.toastService.displayErrorToast('Could not save category', error);
-          // Optionally, you can handle the error in more detail or display a message to the user.
-        },
-      });
+        .upsertCategory(this.categoryForm.value)
+        .pipe(finalize(() => (this.submitting = false)))
+        .subscribe({
+          next: () => {
+            this.toastService.displaySuccessToast('Category saved');
+            this.modalCtrl.dismiss(null, 'refresh');
+          },
+          error: (error) => this.toastService.displayErrorToast('Could not save category', error),
+        });
   }
-
-
   delete(): void {
     from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this category?'))
-      .pipe(filter((action) => action === 'delete'))
-      .subscribe({
-        next: () => {
-          this.modalCtrl.dismiss(null, 'delete');
-        },
-      });
+        .pipe(filter((action) => action === 'delete'))
+        .subscribe({
+          next: () => {
+            this.modalCtrl.dismiss(null, 'delete');
+          },
+        });
   }
 }
