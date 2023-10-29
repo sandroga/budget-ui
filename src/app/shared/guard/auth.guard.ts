fcc655edf1cc9c2af0ca0c22d836c77b0c87f27a
San3 <98056078+sandroga@users.noreply.github.com>
2023-10-29 11:52:10 +0100

    Pfad Anpassung

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,22 +1,21 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from '@angular/router';
 import {map, Observable} from 'rxjs';
-import {AuthService} from 'src/app/shared/service/auth.service';
-import {loginPath} from 'src/app/shared/routes';
+import {AuthService} from '../service/auth.service';
+import {loginPath} from '../routes';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly router: Router,
-  ) {}
-
-  canActivate = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> =>
-    this.authService.currentUser().pipe(
-      map((user) => {
-        if (!user)
-          this.router.navigate([`/${loginPath}`], state.url !== '/' ? { queryParams: { returnUrl: state.url } } : {});
-        return !!user;
-      }),
-    );
+    constructor(
+        private readonly authService: AuthService,
+        private readonly router: Router,
+    ) {}
+    canActivate = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> =>
+        this.authService.currentUser().pipe(
+            map((user) => {
+                if (!user)
+                    this.router.navigate([`/${loginPath}`], state.url !== '/' ? { queryParams: { returnUrl: state.url } } : {});
+                return !!user;
+            }),
+        );
 }
