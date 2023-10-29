555be92c9eda87df0d94b4190ac6238f4b01bf3a
San3 <98056078+sandroga@users.noreply.github.com>
2023-10-29 11:55:46 +0100

    Pfad Anpassung

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,32 +1,32 @@
 import {Component} from '@angular/core';
-import {AuthService} from 'src/app/shared/service/auth.service';
+import {AuthService} from '../service/auth.service';
 import {ActivatedRoute, Router} from '@angular/router';
 
 interface LoginProvider {
-  name: string;
-  logo: string;
-  loginMethod: (callback: () => Promise<boolean>) => void;
+    name: string;
+    logo: string;
+    loginMethod: (callback: () => Promise<boolean>) => void;
 }
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
+    selector: 'app-login',
+    templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  readonly loginProviders: LoginProvider[] = [
-    { name: 'GitHub', logo: 'logo-github', loginMethod: this.authService.loginWithGithub },
-    { name: 'Google', logo: 'logo-google', loginMethod: this.authService.loginWithGoogle },
-  ];
+    readonly loginProviders: LoginProvider[] = [
+        { name: 'GitHub', logo: 'logo-github', loginMethod: this.authService.loginWithGithub },
+        { name: 'Google', logo: 'logo-google', loginMethod: this.authService.loginWithGoogle },
+    ];
 
-  constructor(
-    private readonly activatedRoute: ActivatedRoute,
-    private readonly authService: AuthService,
-    private readonly router: Router
-  ) {}
+    constructor(
+        private readonly activatedRoute: ActivatedRoute,
+        private readonly authService: AuthService,
+        private readonly router: Router
+    ) {}
 
-  login = (loginProvider: LoginProvider): void => {
-    const returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] ?? '/';
-    loginProvider.loginMethod(() => this.router.navigateByUrl(returnUrl));
-  };
+    login = (loginProvider: LoginProvider): void => {
+        const returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] ?? '/';
+        loginProvider.loginMethod(() => this.router.navigateByUrl(returnUrl));
+    };
 
 }
