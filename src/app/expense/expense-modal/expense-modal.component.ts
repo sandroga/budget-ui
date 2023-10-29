8be57a5127e2e144adc9c38016f141a12950c120
San3 <98056078+sandroga@users.noreply.github.com>
2023-10-29 11:33:27 +0100

    überprüfen aufgrund debugging

--- src/app/expense/expense-modal/expense-modal.component.ts
+++ src/app/expense/expense-modal/expense-modal.component.ts
@@ -10,24 +10,20 @@ import {ActionSheetService} from '../../shared/service/action-sheet.service';
 })
 export class ExpenseModalComponent {
   constructor(
-    private readonly actionSheetService: ActionSheetService,
-    private readonly modalCtrl: ModalController,
+      private readonly actionSheetService: ActionSheetService,
+      private readonly modalCtrl: ModalController,
   ) {}
-
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
   }
-
   save(): void {
     this.modalCtrl.dismiss(null, 'save');
   }
-
   delete(): void {
     from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this expense?'))
-      .pipe(filter((action) => action === 'delete'))
-      .subscribe(() => this.modalCtrl.dismiss(null, 'delete'));
+        .pipe(filter((action) => action === 'delete'))
+        .subscribe(() => this.modalCtrl.dismiss(null, 'delete'));
   }
-
   async showCategoryModal(): Promise<void> {
     const categoryModal = await this.modalCtrl.create({ component: CategoryModalComponent });
     categoryModal.present();
