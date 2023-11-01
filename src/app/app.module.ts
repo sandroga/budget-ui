beea58f1b99e69d42ccd6c654605e8d45db08b6e
San3 <98056078+sandroga@users.noreply.github.com>
2023-11-01 21:33:15 +0100

    pwa updated successfull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouteReuseStrategy} from '@angular/router';
-import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import { DEFAULT_CURRENCY_CODE, isDevMode, LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import locale from '@angular/common/locales/en-CH';
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {registerLocaleData} from '@angular/common';
-import {AngularFireModule} from '@angular/fire/compat';
-import {AngularFireAuthModule} from '@angular/fire/compat/auth';
-import {AngularFireStorageModule} from '@angular/fire/compat/storage';
-import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
-import {environment} from '../environments/environment';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {LoginComponent} from './shared/login/login.component';
-import {AuthInterceptor} from './shared/interceptor/auth.interceptor';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { registerLocaleData } from '@angular/common';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './shared/login/login.component';
+import { AuthInterceptor } from './shared/interceptor/auth.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 registerLocaleData(locale);
 
@@ -28,6 +29,12 @@ registerLocaleData(locale);
     BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'CHF' },
