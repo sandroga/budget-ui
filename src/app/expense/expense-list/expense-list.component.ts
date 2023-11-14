eb0fa92e67727d99caaf92909a1ef5e7144da02a
San3 <98056078+sandroga@users.noreply.github.com>
2023-11-14 23:13:57 +0100

    Diverses bei Ausgaben suchen und sortieren debbuged

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { debounce, interval } from 'rxjs';
+import { debounce, finalize, interval, Subscription } from 'rxjs';
 import { Expense, ExpenseCriteria, SortOption } from '../../shared/domain';
 import { ExpenseService } from '../expense.service';
 import { ToastService } from '../../shared/service/toast.service';
@@ -20,9 +20,10 @@ interface ExpenseGroup {
 })
 export class ExpenseListComponent {
   expenseGroups: ExpenseGroup[] | null = null;
-  loading = false;
   expenses: Expense[] | null = null;
+  readonly initialSort = 'name,asc';
   lastPageReached = false;
+  loading = false;
   searchCriteria: ExpenseCriteria = { page: 0, size: 25, sort: 'name,asc' };
   date = new Date(); // Aktuelles Datum
   readonly searchForm: FormGroup;
@@ -30,10 +31,13 @@ export class ExpenseListComponent {
     { label: 'Created at (newest first)', value: 'createdAt,desc' },
     { label: 'Created at (oldest first)', value: 'createdAt,asc' },
     { label: 'Date (newest first)', value: 'createdAt,asc' },
-    { label: 'Date (oldest first)', value: 'createdAt,asc' },
+    { label: 'Date (oldest first)', value: 'createdAt,desc' },
     { label: 'Name (A-Z)', value: 'name,asc' },
     { label: 'Name (Z-A)', value: 'name,desc' },
   ];
+
+  private readonly searchFormSubscription: Subscription;
+
   constructor(
     private readonly modalCtrl: ModalController,
     private readonly formBuilder: FormBuilder,
@@ -41,8 +45,9 @@ export class ExpenseListComponent {
     private readonly toastService: ToastService,
     private readonly categoryService: CategoryService,
   ) {
-    this.searchForm = this.formBuilder.group({ name: [], sort: ['name,asc'] });
+    this.searchForm = this.formBuilder.group({ name: [], sort: [this.initialSort] });
     this.searchForm.valueChanges
+    this.searchFormSubscription = this.searchForm.valueChanges
       .pipe(debounce((value) => interval(value.name?.length ? 400 : 0)))
       .subscribe((value) => {
         this.searchCriteria = { ...this.searchCriteria, ...value, page: 0 };
@@ -53,32 +58,40 @@ export class ExpenseListComponent {
   async openModal(expense?: Expense): Promise<void> {
     const modal = await this.modalCtrl.create({
       component: ExpenseModalComponent,
-      componentProps: { expense: expense ? { ...expense } : {} },
     });
     modal.present();
     const { role } = await modal.onWillDismiss();
-    console.log('role', role);
+    if (role === 'refresh') this.reloadExpenses();
   }
+  ionViewDidEnter(): void {
+    this.loadCategories();
+  }
+  ionViewDidLeave(): void {
+    this.searchFormSubscription.unsubscribe();
+  }
+
   private loadExpenses(next: () => void = () => {}): void {
+    if (!this.searchCriteria.categoryIds?.length) delete this.searchCriteria.categoryIds;
     if (!this.searchCriteria.name) delete this.searchCriteria.name;
     this.loading = true;
     this.expenseService
       .getExpenses(this.searchCriteria)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+          next();
+        }),
+      )
       .subscribe({
         next: (expenses) => {
           if (this.searchCriteria.page === 0 || !this.expenses) this.expenses = [];
-
           this.expenses.push(...expenses.content);
           this.lastPageReached = expenses.last;
         },
         error: (error) => this.toastService.displayErrorToast('Could not load expenses', error),
-        complete: () => {
-          this.loading = false;
-          next();
-        },
       });
   }
-  loadNextExpensePage($event: any) {
+  loadNextExpensesPage($event: any) {
     this.searchCriteria.page++;
     this.loadExpenses(() => ($event as InfiniteScrollCustomEvent).target.complete());
   }
@@ -92,7 +105,11 @@ export class ExpenseListComponent {
     this.loadExpenses();
   }
   // Methode zum Anzeige vom aktuellen Datums im Titel
+  categories: any;
+  Expense: Expense | undefined;
   getCurrentMonthYear(): string {
     return format(this.date, 'MMMM yyyy');
   }
+
+  private loadCategories() {}
 }
