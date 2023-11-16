961df045428c6c40ddb6f4a802fd53f445964122
San3 <98056078+sandroga@users.noreply.github.com>
2023-11-16 11:56:19 +0100

    new_ExpenseService

--- src/app/expense/expense.service.ts
+++ src/app/expense/expense.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Expense, ExpenseUpsertDto, Page, PagingCriteria } from '../shared/domain';
+import { Expense, ExpenseCriteria, Page } from '../shared/domain';
 import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ExpenseService {
   private readonly apiUrl = `${environment.backendUrl}/expenses`;
+  private readonly apiV2Url = `${environment.backendUrl}/v2/expenses`;
 
   constructor(private readonly httpClient: HttpClient) {}
 
   // Read
-  getExpenses = (pagingCriteria: PagingCriteria): Observable<Page<Expense>> =>
+
+  getExpenses = (pagingCriteria: ExpenseCriteria): Observable<Page<Expense>> =>
     this.httpClient.get<Page<Expense>>(this.apiUrl, { params: new HttpParams({ fromObject: { ...pagingCriteria } }) });
 
+
   // Create & Update
-  upsertExpense = (expense: ExpenseUpsertDto): Observable<void> => this.httpClient.put<void>(this.apiUrl, expense);
+
+  upsertExpense = (expenses : Expense): Observable<void> => this.httpClient.put<void>(this.apiUrl, expenses);
 
   // Delete
+
   deleteExpense = (id: string): Observable<void> => this.httpClient.delete<void>(`${this.apiUrl}/${id}`);
 }
+
+
