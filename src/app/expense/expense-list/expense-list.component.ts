b11662a399899ef65d74968e07c1dc4ca03ff5c8
San3 <98056078+sandroga@users.noreply.github.com>
2023-11-16 13:35:23 +0100

    new_ Kategorie anzeigen

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { addMonths, set } from 'date-fns';
-import { ModalController } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
-import { Expense } from '../../shared/domain';
+import { Expense, ExpenseCriteria } from '../../shared/domain';
+import { ToastService } from '../../shared/service/toast.service';
+import { ExpenseService } from '../expense.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-expense-overview',
@@ -10,12 +13,42 @@ import { Expense } from '../../shared/domain';
 })
 export class ExpenseListComponent {
   date = set(new Date(), { date: 1 });
+  expenses: Expense[] | null = null;
+  readonly initialSort = 'name,asc';
+  lastPageReached = false;
+  loading = false;
+  searchCriteria: ExpenseCriteria = { page: 0, size: 25, sort: this.initialSort };
 
-  constructor(private readonly modalCtrl: ModalController) {}
-
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly expenseService: ExpenseService,
+    private readonly toastService: ToastService,
+  ) {}
   addMonths = (number: number): void => {
     this.date = addMonths(this.date, number);
   };
+
+  private loadExpenses(next: () => void = () => {}): void {
+    if (!this.searchCriteria.name) delete this.searchCriteria.name;
+    this.loading = true;
+    this.expenseService
+      .getExpenses(this.searchCriteria)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+          next();
+        }),
+      )
+      .subscribe({
+        next: (expenses) => {
+          if (this.searchCriteria.page === 0 || !this.expenses) this.expenses = [];
+          this.expenses.push(...expenses.content);
+          this.lastPageReached = expenses.last;
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not load expenses', error),
+      });
+  }
+
   async openModal(expense?: Expense): Promise<void> {
     const modal = await this.modalCtrl.create({
       component: ExpenseModalComponent,
@@ -23,6 +56,18 @@ export class ExpenseListComponent {
     });
     modal.present();
     const { role } = await modal.onWillDismiss();
+    if (role === 'refresh') this.reloadExpenses();
     console.log('role', role);
+  }
+  ionViewDidEnter(): void {
+    this.loadExpenses();
+  }
+  loadNextExpensePage($event: any) {
+    this.searchCriteria.page++;
+    this.loadExpenses(() => ($event as InfiniteScrollCustomEvent).target.complete());
+  }
+  reloadExpenses($event?: any): void {
+    this.searchCriteria.page = 0;
+    this.loadExpenses(() => ($event ? ($event as RefresherCustomEvent).target.complete() : {}));
   }
 }
