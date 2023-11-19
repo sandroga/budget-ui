45e34af848c1f5570cd58d68d535b6886b7b1852
San3 <sandro.gamito@lernen.siw.swiss>
2023-11-19 17:45:44 +0100

    Troubleshooting expense-list ts+html_works!

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { addMonths, set } from 'date-fns';
 import { InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
 import { Category, Expense, ExpenseCriteria, SortOption } from '../../shared/domain';
 import { ToastService } from '../../shared/service/toast.service';
 import { ExpenseService } from '../expense.service';
-import { debounce, finalize, from, groupBy, interval, mergeMap, toArray } from 'rxjs';
+import { BehaviorSubject, debounce, from, groupBy, interval, mergeMap, Subject, takeUntil, toArray } from 'rxjs';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CategoryService } from '../../category/category.service';
 import { formatPeriod } from '../../shared/period';
@@ -20,14 +20,16 @@ interface ExpenseGroup {
   templateUrl: './expense-list.component.html',
 })
 
-export class ExpenseListComponent {
+export class ExpenseListComponent implements OnInit, OnDestroy {
   date = set(new Date(), { date: 1 });
-  expenses: Expense[] | null = null;
   expenseGroups: ExpenseGroup[] | null = null;
   readonly initialSort = 'name,asc';
   lastPageReached = false;
   loading = false;
   searchCriteria: ExpenseCriteria = { page: 0, size: 25, sort: this.initialSort };
+
+  categories: Category[] = [];
+
   readonly searchForm: FormGroup;
   readonly sortOptions: SortOption[] = [
     { label: 'Created at (newest first)', value: 'createdAt,desc' },
@@ -38,7 +40,7 @@ export class ExpenseListComponent {
     { label: 'Date (oldest first)', value: 'name,desc' },
   ];
 
-  categories: Category[] = [];
+  private readonly unsubscribe = new Subject<void>();
 
   constructor(
     private readonly modalCtrl: ModalController,
@@ -49,7 +51,9 @@ export class ExpenseListComponent {
   ) {
     this.searchForm = this.formBuilder.group({ categoryIds: [], name: [], sort: [this.initialSort] });
     this.searchForm.valueChanges
-      .pipe(debounce((value) => interval(value.name?.length ? 400 : 0)))
+      .pipe(
+        takeUntil(this.unsubscribe),
+        debounce((value) => interval(value.name?.length ? 400 : 0)))
       .subscribe((value) => {
         this.searchCriteria = { ...this.searchCriteria, ...value, page: 0 };
         this.loadExpenses();
@@ -57,6 +61,7 @@ export class ExpenseListComponent {
   }
   addMonths = (number: number): void => {
     this.date = addMonths(this.date, number);
+    this.reloadExpenses();
   };
 
   private loadExpenses(next: () => void = () => {}): void {
@@ -64,22 +69,19 @@ export class ExpenseListComponent {
     if (!this.searchCriteria.categoryIds?.length) delete this.searchCriteria.categoryIds;
     if (!this.searchCriteria.name) delete this.searchCriteria.name;
     this.loading = true;
-    const groupByDate = this.searchCriteria.sort.startsWith('date');
     this.expenseService
       .getExpenses(this.searchCriteria)
       .pipe(
-        finalize(() => (this.loading = false)),
         mergeMap((expensePage) => {
           this.lastPageReached = expensePage.last;
           next();
-          if (this.searchCriteria.page === 0 || !this.expenseGroups) this.expenseGroups = [];
-          return from(expensePage.content)
-      .pipe(
-            groupBy((expense) => (groupByDate ? expense.date : expense.id)),
-            mergeMap((group) => group
-              .pipe(toArray())),
+          this.loading = false;
+          if (this.searchCriteria.page === 0 || this.expenseGroups) this.expenseGroups = [];
+          return from(expensePage.content).pipe(
+            groupBy((expense) => expense.date),
+            mergeMap((group) => group.pipe(toArray()))
           );
-        }),
+        })
       )
       .subscribe({
         next: (expenses: Expense[]) => {
@@ -88,34 +90,31 @@ export class ExpenseListComponent {
             expenses: this.sortExpenses(expenses),
           };
           const expenseGroupWithSameDate = this.expenseGroups!.find((other) => other.date === expenseGroup.date);
-          if (!expenseGroupWithSameDate || !groupByDate) this.expenseGroups!.push(expenseGroup);
+          if (!expenseGroupWithSameDate) this.expenseGroups!.push(expenseGroup);
           else
             expenseGroupWithSameDate.expenses = this.sortExpenses([
               ...expenseGroupWithSameDate.expenses,
               ...expenseGroup.expenses,
             ]);
         },
-        error: (error) => this.toastService.displayErrorToast('Could not load expenses', error),
+        error: (error) => {
+          this.toastService.displayErrorToast('Could not load expenses', error);
+          this.loading = false;
+        },
       });
   }
 
   private sortExpenses = (expenses: Expense[]): Expense[] => expenses.sort((a, b) => a.name.localeCompare(b.name));
-  private loadCategories(next: () => void = () => {}): void {
-    if (!this.searchCriteria.name) delete this.searchCriteria.name;
-    this.loading = true;
-    this.categoryService
-      .getCategories(this.searchCriteria)
-      .pipe(
-        finalize(() => {
-          this.loading = false;
-          next();
-        }),
-      )
+
+  private loadAllCategories(): void {
+    const pageToLoad = new BehaviorSubject(0);
+    pageToLoad
+      .pipe(mergeMap((page) => this.categoryService.getCategories({ page, size: 25, sort: 'name,asc'})))
       .subscribe({
         next: (categories) => {
-          if (this.searchCriteria.page === 0 || !this.categories) this.categories = [];
+          if (pageToLoad.value === 0) this.categories = [];
           this.categories.push(...categories.content);
-          this.lastPageReached = categories.last;
+          if (!categories.last) pageToLoad.next(pageToLoad.value + 1);
         },
         error: (error) => this.toastService.displayErrorToast('Could not load categories', error),
       });
@@ -128,8 +127,16 @@ export class ExpenseListComponent {
     });
     modal.present();
     const { role } = await modal.onWillDismiss();
-    if (role === 'refresh') this.reloadExpenses();
+    if (role === 'refresh') this.loadExpenses();
     console.log('role', role);
+  }
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+  ngOnInit(): void {
+    this.loadExpenses();
+    this.loadAllCategories();
   }
   ionViewWillEnter(): void {
     this.loadExpenses();
--- src/app/expense/expense.service.ts
+++ src/app/expense/expense.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { AllCategoryCriteria, Expense, ExpenseCriteria, ExpenseUpsertDto, Page } from '../shared/domain';
+import { Category, Expense, ExpenseUpsertDto, Page, PagingCriteria } from '../shared/domain';
 import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ExpenseService {
   private readonly apiUrl = `${environment.backendUrl}/expenses`;
-  private readonly apiV2Url = `${environment.backendUrl}/v2/expenses`;
 
   constructor(private readonly httpClient: HttpClient) {}
 
   // Read
 
-  getExpenses = (pagingCriteria: ExpenseCriteria): Observable<Page<Expense>> =>
+  getExpenses = (pagingCriteria: PagingCriteria): Observable<Page<Expense>> =>
     this.httpClient.get<Page<Expense>>(this.apiUrl, { params: new HttpParams({ fromObject: { ...pagingCriteria } }) });
 
-  getAllExpenses = (sortCriteria: AllCategoryCriteria): Observable<Expense[]> =>
-    this.httpClient.get<Expense[]>(this.apiV2Url, { params: new HttpParams({ fromObject: { ...sortCriteria } }) });
+  getAllExpenses = (sortCriteria: PagingCriteria): Observable<Expense[]> =>
+    this.httpClient.get<Expense[]>(this.apiUrl, { params: new HttpParams({ fromObject: { ...sortCriteria } }) });
 
   // Create & Update
 
-  upsertExpense = (expenseDto: ExpenseUpsertDto): Observable<Expense> =>
-    this.httpClient.put<Expense>(this.apiUrl, expenseDto);
+  upsertExpense = (expense: ExpenseUpsertDto): Observable<void> =>
+    this.httpClient.put<void>(this.apiUrl, expense);
 
   // Delete
 
